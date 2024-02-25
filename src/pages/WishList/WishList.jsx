eb977068e6a2816121b5785306fef3a2b480b352
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-02-25 02:50:54 +0200

    🛒 feat:fetching wishlist from api

--- src/pages/WishList/WishList.jsx
+++ src/pages/WishList/WishList.jsx
@@ -1,6 +1,20 @@
-
+import axios from "axios"
+import { useQuery } from "react-query"
+import Loader from "../../components/Loader/Loader"
+import { baseUrl } from "../../api/api"
 
 function WishList() {
+  function getWishList() {
+    return axios.get(baseUrl + "/api/v1/wishlist", {
+      headers: {
+        token: localStorage.getItem('token'),
+    }});
+  }
+
+  const { data, isLoading } = useQuery('getWishList', getWishList);
+  console.log(data)
+
+  if (isLoading) return <Loader />;
   return (
     <div>WishList</div>
   )
